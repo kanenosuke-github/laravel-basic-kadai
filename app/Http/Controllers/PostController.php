9f4cfadda18a2a6b500a047cf15189899b546eda
kanenosuke-github <shun.216.nosuke@gmail.com>
2024-05-24 12:29:06 +0900

    laravel-basic-kadai-create

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -17,9 +17,34 @@ class PostController extends Controller
 
     public function show($id) {
         $post = Post::find($id);
+
+        if(!$post){
+            abort(404,'Post not found');
+        }
     
         return view('posts.show',compact('post'));
     }
+
+    public function create() {
+        return view('posts.create');
+    }
+
+    public function store(Request $request) {
+        // バリデーションを設定する
+        $request->validate([
+            'title' => 'required|max:20',
+            'content' => 'required|max:200'
+        ]);
+
+        // フォームの入力内容をもとに、テーブルにデータを追加する
+        $post = new Post();
+        $post->title = $request->input('title');
+        $post->content = $request->input('content');
+        $post->save();
+
+        // リダイレクトさせる
+        return redirect("/posts/{$post->id}");
+    }      
 }
 
 
--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,7 @@ Route::get('/', function () {
 Route::get('/posts',[PostController::class,'index']);
 
 Route::get('/posts/{id}',[PostController::class,'show']);
+
+Route::get('/posts/create',[PostController::class,'create']);
+
+Route::get('/posts/store',[PostController::class,'store'])->name('posts.store');
